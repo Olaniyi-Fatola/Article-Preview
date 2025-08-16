346c99e6f693a52ecd0ced511b6af67da85a2f66
Olaniyi-Fatola <olhaniyi08@gmail.com>
2025-08-16 08:26:08 -0400

    updated

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var shares = document.getElementById(share)
-var social = document.getElementById(socials)
-var button = document.getElementById(share-icon)
+var share = document.getElementById("share")
+var social = document.getElementById("social")
+var button = document.getElementById("share-icon")
+var profile = document.getElementById("profile")
 
 button.onclick = function(){
 
-    if (share.className=="open"){
+    if (share.className=="visible"){
         //social media button disappear
         share.className="";
+        profile.className="";
+        button.className="";
     }
     else{
         //else social media button appear
-        share.className="open";
+        share.className="visible";
+        profile.className="hidden";
+        button.className="open";
     }
 
 };
